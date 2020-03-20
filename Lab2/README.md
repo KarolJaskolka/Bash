9c066d65df24f7b301edf31251ea442f16630d2e
KarolJaskolka <jaskolka.karol@gmail.com>
2020-03-20 11:41:30 +0100

    add lab2_1

---
+++ Lab2/README.md
@@ -0,0 +1,4 @@
+Zad 1
+
+Napisz skrypt, który dla wszystkich plików wykonywalnych z jakiegoś katalogu (parametr wywołania skryptu) utworzy dowiązania twarde w drugim katalogu (parametr wywołania skryptu), zaś dla podkatalogów utworzy on dowiązania miękkie. 
+ 
---
+++ Lab2/lab2_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Karol Jaskolka
+# Zadanie 22
+
+if [ $# -ne 2 ]
+    exit 1
+fi
+
+for plik in $( ls $1 )
+do
+    if [ -x $1/$plik -a -f $1/$plik ]
+    then
+        ln $1/$plik $2/$plik
+    elif [ -d $1/$plik ]
+    then
+        ln -s $1/$plik $2/$plik
+    fi
+done
